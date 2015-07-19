5635f61f72661962fbff46f94d4a343f618be4d9
Jeff Cooper <jeff@cuperman.net>
2015-07-19 15:03:03 -0700

    Fix default attributes

--- steamcmd/attributes/default.rb
+++ steamcmd/attributes/default.rb
@@ -2,4 +2,4 @@ default[:steamcmd][:user] = 'steam'
 default[:steamcmd][:group] = 'steam'
 default[:steamcmd][:home] = "/home/#{node[:steamcmd][:user]}"
 default[:steamcmd][:steamcmd_dir] = '/opt/steamcmd'
-default[:steamcmd][:apps_dir] = "#{node[:steamcmd][:steamcmd_dir]/apps}"
+default[:steamcmd][:apps_dir] = "#{node[:steamcmd][:steamcmd_dir]}/apps"
