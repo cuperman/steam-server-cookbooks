baf28ce4c9127aacd1953fb6e907cc89666a7a61
Jeff Cooper <jeff@cuperman.net>
2015-07-22 22:54:02 -0700

    More tests

--- steamcmd/spec/csgo_spec.rb
+++ steamcmd/spec/csgo_spec.rb
@@ -5,4 +5,16 @@ describe 'steamcmd::csgo' do
   
   it { expect(chef_run).to include_recipe('steamcmd::default') }
   it { expect(chef_run).to create_template('/opt/steamcmd/scripts/csgo.txt') }
+  it { expect(chef_run).to render_file('/opt/steamcmd/scripts/csgo.txt').with_content('force_install_dir /opt/steamcmd/apps/csgo') }
+  it { expect(chef_run).to render_file('/opt/steamcmd/scripts/csgo.txt').with_content('app_update 740 validate') }
+  it { expect(chef_run).to run_execute('/opt/steamcmd/steamcmd.sh +runscript /opt/steamcmd/scripts/csgo.txt') }
+  it { expect(chef_run).to create_template('/etc/init.d/csgo') }
+  it { expect(chef_run).to render_file('/etc/init.d/csgo').with_content("TITLE='Source Dedicated Server'") }
+  it { expect(chef_run).to render_file('/etc/init.d/csgo').with_content("LONGNAME='Counter-Strike: GO'") }
+  it { expect(chef_run).to render_file('/etc/init.d/csgo').with_content("NAME='csgo'") }
+  it { expect(chef_run).to render_file('/etc/init.d/csgo').with_content("DAEMON='srcds_run'") }
+  it { expect(chef_run).to render_file('/etc/init.d/csgo').with_content("STEAM='/opt/steamcmd/apps/csgo'") }
+  it { expect(chef_run).to render_file('/etc/init.d/csgo').with_content("USER='steam'") }
+  it { expect(chef_run).to start_service('csgo') }
+  it { expect(chef_run).to enable_service('csgo') }
 end
--- steamcmd/spec/default_spec.rb
+++ steamcmd/spec/default_spec.rb
@@ -3,6 +3,14 @@ require 'chefspec'
 describe 'steamcmd::default' do
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
   
-  it { expect(chef_run).to install_yum_package('glibc') }
-  it { expect(chef_run).to install_yum_package('libstdc++') }
+  it { expect(chef_run).to install_yum_package('glibc').with(arch: 'i686') }
+  it { expect(chef_run).to install_yum_package('libstdc++').with(arch: 'i686') }
+  it { expect(chef_run).to create_group('steam') }
+  it { expect(chef_run).to create_user('steam').with(gid: 'steam', home: '/home/steam') }
+  it { expect(chef_run).to create_directory('/opt/steamcmd').with(mode: '0755') }
+  it { expect(chef_run).to create_directory('/opt/steamcmd/downloads').with(mode: '0755') }
+  it { expect(chef_run).to create_directory('/opt/steamcmd/scripts').with(mode: '0755') }
+  it { expect(chef_run).to create_directory('/opt/steamcmd/apps').with(mode: '0755') }
+  it { expect(chef_run).to create_remote_file('/opt/steamcmd/downloads/steamcmd_linux.tar.gz').with(mode: '0644') }
+  it { expect(chef_run).to run_execute('tar zxvf /opt/steamcmd/downloads/steamcmd_linux.tar.gz') }
 end
